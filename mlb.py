75ee7335d2e72feca7ecd194881008ed5e25bfe1
Stephen Ou <me@stephenou.com>
2015-07-13 22:29:25 -0700

    Include all the team names

---
+++ mlb.py
@@ -0,0 +1,50 @@
+teams = {
+	"American League": {
+		"East": [
+			"Baltimore Orioles",
+			"Boston Red Sox",
+			"New York Yankees",
+			"Tampa Bay Rays",
+			"Toronto Blue Jays"
+		],
+		"Central": [
+			"Chicago White Sox",
+			"Cleveland Indians",
+			"Detroit Tigers",
+			"Kansas City Royals",
+			"Minnesota Twins"
+		],
+		"West": [
+			"Anaheim Angels",
+			"Houston Astros",
+			"Oakland Athletics",
+			"Seattle Mariners",
+			"Texas Rangers"
+		]
+	},
+	"National League": {
+		"East": [
+			"Atlanta Braves",
+			"Miami Marlins",
+			"New York Mets",
+			"Philadelphia Phillies",
+			"Washington Nationals"
+		],
+		"Central": [
+			"Chicago Cubs",
+			"Cincinnati Reds",
+			"Milwaukee Brewers",
+			"Pittsburgh Pirates",
+			"St. Louis Cardinals"
+		],
+		"West": [
+			"Arizona Diamondbacks",
+			"Colorado Rockies",
+			"Los Angeles Dodgers",
+			"San Diego Padres",
+			"San Francisco Giants"
+		]
+	}
+}
+
+print teams
